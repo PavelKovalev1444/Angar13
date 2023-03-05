4538c80332261c7da348970466292e7d814fb521
Pavel Kovalev <pavel.kovalev30@gmail.com>
2023-03-05 23:48:40 +0300

    Add files via upload

---
+++ Artificial Systems/lab2/app.js
@@ -0,0 +1,35 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+const Agent = require('./agent')
+const VERSION = 7
+
+function createAgent(coords, rotationSpeed, teamName, position){
+  let agent = new Agent(rotationSpeed, position, teamName)
+  require('./socket')(agent, teamName, VERSION)
+  if(position === 'l'){
+    let leftCoords = coords.split(' ')
+    if(leftCoords[0][0] !== '-'){
+      leftCoords[0] = '-' + leftCoords[0]
+    }
+    leftCoords = leftCoords.join(' ')
+    setTimeout(() => agent.socketSend("move", leftCoords), 200)
+  }else{
+    let rightCoords = coords.split(' ')
+    if(rightCoords[0][0] !== '-'){
+      rightCoords[0] = '-' + rightCoords[0]
+    }
+    rightCoords = rightCoords.join(' ')
+    setTimeout(() => agent.socketSend("move", rightCoords), 200)
+  }
+}
+
+rl.question('Начальные координаты игрока: ', (coordsPlayer) => {
+  rl.question('Скорость вращения игрока: ', (rotationSpeedPlayer) => {
+    rl.question('Имя команды игрока: ', (teamNamePlayer) => {
+      createAgent(coordsPlayer, rotationSpeedPlayer, teamNamePlayer, 'l')
+    })
+  })
+});
